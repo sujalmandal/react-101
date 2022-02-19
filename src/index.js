bd6b1d5e994e5f99ef353fdb6b46f0753b678914
Sujal Mandal <ss6sujal@gmail.com>
2022-02-19 14:06:55 +0400

    styling example

--- src/index.js
+++ src/index.js
@@ -3,34 +3,46 @@ import ReactDOM from 'react-dom';
 
 const NavItemsComponent = function(){
     return (
-    <ul>
+    <ul className ="nav-list-items">
         <li>Pricing</li>
         <li>Contact</li>
         <li>About Us</li>
     </ul>);
 }
 
-const NavBarComponent =function(){
+const NavBarComponent = function(){
     return (
-        <nav>
-            <h1>Hotel 101 </h1>
-            <NavItemsComponent/>
-        </nav>
+        <header>
+            <nav className="nav">
+                <NavItemsComponent/>
+            </nav>
+        </header>
     );
 };
 
-/*
+const MainComponent = function(){
+    return (
+        <div>
+            <h1>Hello, world.</h1>
+        </div>
+    );
+}
+
+const FooterComponent = function(){
+    return (
+        <footer className="footer">
+            <small>© Sujal Mandal - 2022 </small>
+        </footer>
+    );
+}
+
+const rootElem = document.getElementById("root");
+
 ReactDOM.render(
     <div>
     <NavBarComponent/>
-    <p>Copyright 2100 (c)</p>
+    <MainComponent/>
+    <FooterComponent/>
     </div>,
-    //selector
-    document.getElementById("root")
+    rootElem
 );
-*/
-
-const jsxElemDescriptor = <div>hello cruel world</div>;
-console.log(jsxElemDescriptor);
-const rootElem = document.getElementById("root");
-rootElem.append(jsxElemDescriptor);
